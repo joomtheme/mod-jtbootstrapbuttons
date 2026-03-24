16d1c05afbbd798bf194c4d819bccd6001084524
Ugur Uygur <support@joomtheme.com>
2026-03-24 11:11:19 +0300

    Add files via upload

--- services/provider.php
+++ services/provider.php
@@ -1,1 +1,28 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_jtbootstrapbuttons
+ *
+ * @author      JoomTheme <support@joomtheme.com>
+ * @copyright   (C) 2026 JoomTheme
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @link        https://joomtheme.com
+ * @since       1.0.2
+ */
 
+defined('_JEXEC') || die;
+
+use Joomla\CMS\Extension\Service\Provider\HelperFactory;
+use Joomla\CMS\Extension\Service\Provider\Module;
+use Joomla\CMS\Extension\Service\Provider\ModuleDispatcherFactory;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class () implements ServiceProviderInterface {
+    public function register(Container $container): void
+    {
+        $container->registerServiceProvider(new ModuleDispatcherFactory('\\JoomTheme\\Module\\BootstrapButtons'));
+        $container->registerServiceProvider(new HelperFactory('\\JoomTheme\\Module\\BootstrapButtons\\Site\\Helper'));
+        $container->registerServiceProvider(new Module());
+    }
+};
