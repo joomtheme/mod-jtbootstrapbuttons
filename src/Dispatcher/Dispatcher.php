20f55ed47f431cce98ece22e82d36afed0e07278
Ugur Uygur <support@joomtheme.com>
2026-03-24 11:09:15 +0300

    Add files via upload

--- src/Dispatcher/Dispatcher.php
+++ src/Dispatcher/Dispatcher.php
@@ -1,1 +1,35 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_jtbootstrapbuttons
+ *
+ * @author      JoomTheme <support@joomtheme.com>
+ * @copyright   (C) 2026 JoomTheme
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @link        https://joomtheme.com
+ * @since       1.0.2
+ */
 
+namespace JoomTheme\Module\BootstrapButtons\Site\Dispatcher;
+
+defined('_JEXEC') || die;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+final class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+        $helper = $this->getHelperFactory()->getHelper('ButtonsHelper');
+
+        $data['buttons'] = $helper->getButtons($data['params']);
+        $data['wrapperClass'] = $helper->getWrapperClass($data['params']);
+
+        return $data;
+    }
+}
